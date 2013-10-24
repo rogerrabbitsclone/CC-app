d83918cb050755b3340655374e9210abd4aa8a63
rogerrabbitsclone <rogerrabbitsclone@gmail.com>
2013-10-24 12:24:31 -0700

    more comments in code

--- FMS-ODBC.py
+++ FMS-ODBC.py
@@ -1,10 +1,13 @@
 import pyodbc
-cnxn = pyodbc.connect('DRIVER={FileMaker ODBC};SERVER=192.168.1.113:2399;DATABASE=TBS3;UID=rocket;PWD=rocket')
+#as long as the ports are forwarded on the router, you can do this remotly. 
+cnxn = pyodbc.connect('DRIVER={FileMaker ODBC};SERVER=108.254.144.23:2399;DATABASE=TBS3;UID=rocket;PWD=rocket')
 pyodbc.connect
 cursor = cnxn.cursor()
 #to find the table name, open the database in filemaker VIA openremote.
 #then go: file->manage->database. in the "Table" tab you can change the table name
 #filemakers "fields" are SQLs "columns"
+#IE: cursor.execute("select publisher from tobesentsent") is selecting the 'publisher' column from the 'tobesent' table
+#tables cannot have spaces due to pyodbc limitations
 cursor.execute("select publisher from tobesentsent")
 
 row = cursor.fetchall()
